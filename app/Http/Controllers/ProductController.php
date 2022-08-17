a743224aa5a89bd115c83b98b24ff73aac7a11ed
Duncanwar <semunda9@gmail.com>
2022-08-17 15:41:00 +0200

    seizure

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,42 +3,106 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\Product;
 use Illuminate\Http\Request;
-use App\Models\User;
+use Validator;
 
-class AuthController extends Controller
+class ProductController extends Controller
 {
-  public function register(Request $request)
+  /**
+   * Display a listing of the resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function index()
   {
-    $this->validate($request, [
-      'name' => 'required|min:4',
-      'email' => 'required|email',
-      'password' => 'required|min:8',
+    $products = Product::all();
+    return response()->json([
+      "success" => true,
+      "message" => "Product List",
+      "data" => $products
     ]);
-    $user = User::create([
-      'name' => $request->name,
-      'email' => $request->email,
-      'password' => bcrypt($request->password)
-    ]);
-    $token = $user->createToken('Laravel9PassportAuth')->accessToken;
-    return response()->json(['token' => $token], 200);
   }
-  public function login(Request $request)
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return \Illuminate\Http\Response
+   */
+  public function store(Request $request)
   {
-    $data = [
-      'email' => $request->email,
-      'password' => $request->password
-    ];
-    if (auth()->attempt($data)) {
-      $token = auth()->user->createToken('Laravel9PassportAuth')->accessToken;
-      return response()->json(['token' => $token], 200);
-    } else {
-      return response()->json(['error' => "Unauthorised"], 401);
+    $input = $request->all();
+    $validator = Validator::make($input, [
+      'name' => 'required',
+      'detail' => 'required'
+    ]);
+    if ($validator->fails()) {
+      return $this->sendError('Validation Error.', $validator->errors());
     }
+    $product = Product::create($input);
+    return response()->json([
+      "success" => true,
+      "message" => "Product created successfully.",
+      "data" => $product
+    ]);
   }
-  public function userInfo()
+  /**
+   * Display the specified resource.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function show($id)
   {
-    $user = auth()->user();
-    return response()->json(['user' => $user], 200);
+    $product = Product::find($id);
+    if (is_null($product)) {
+      return $this->sendError('Product not found.');
+    }
+    return response()->json([
+      "success" => true,
+      "message" => "Product retrieved successfully.",
+      "data" => $product
+    ]);
+  }
+  /**
+   * Update the specified resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function update(Request $request, Product $product)
+  {
+    $input = $request->all();
+    $validator = Validator::make($input, [
+      'name' => 'required',
+      'detail' => 'required'
+    ]);
+    if ($validator->fails()) {
+      return $this->sendError('Validation Error.', $validator->errors());
+    }
+    $product->name = $input['name'];
+    $product->detail = $input['detail'];
+    $product->save();
+    return response()->json([
+      "success" => true,
+      "message" => "Product updated successfully.",
+      "data" => $product
+    ]);
+  }
+  /**
+   * Remove the specified resource from storage.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function destroy(Product $product)
+  {
+    $product->delete();
+    return response()->json([
+      "success" => true,
+      "message" => "Product deleted successfully.",
+      "data" => $product
+    ]);
   }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\API\ProductController;
+use App\Http\Controllers\API\PassportAuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,14 +16,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
-Route::post('/register', [PassportAuthController::class, 'index']);
-Route::post('/login', [PassportAuthController::class, 'login']);
+
+
+
+Route::post('register', [PassportAuthController::class, 'register']);
+Route::post('login', [PassportAuthController::class, 'login']);
 
 Route::middleware('auth:api')->group(function () {
     Route::get('get-user', [PassportAuthController::class, 'userInfo']);
+
     Route::resource('products', [ProductController::class]);
 });
